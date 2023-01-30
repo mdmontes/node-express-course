ec91a237a564f4594ce9e5d32236789ad8d5c25e
mdmontes <manuel.montes0@gmail.com>
2023-01-30 09:18:40 -0500

    answers for lesson 4

--- 02-express-tutorial/answers/final/012-router-app.js
+++ 02-express-tutorial/answers/final/012-router-app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-// const people  = require('./routes/people')
-const people  = require('./routes/people_2')
-const auth = require('./routes/login')
+// const people  = require('../routes/people')
+const people  = require('../routes/people_2')
+const auth = require('../routes/auth')
 
 
-app.use(express.static('../methods-public'))
+app.use(express.static('../../methods-public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
